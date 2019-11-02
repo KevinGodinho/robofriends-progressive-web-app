9a7293d774ca84e80b90614f994628b32904e835
Katie Godinho <katiegodinho@katies-mbp.home>
2019-11-02 15:46:28 -0700

    Updates

---
+++ precache-manifest.f1451bebe4a3d72f87a5032da08f153e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d17f2394522fca524f1659ed2d13641",
+    "url": "/robofriends-progressive-web-app/index.html"
+  },
+  {
+    "revision": "b687617d559baa4d109a",
+    "url": "/robofriends-progressive-web-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3199cc31b3778ecf904c",
+    "url": "/robofriends-progressive-web-app/static/css/main.23ef247e.chunk.css"
+  },
+  {
+    "revision": "b687617d559baa4d109a",
+    "url": "/robofriends-progressive-web-app/static/js/2.80796f17.chunk.js"
+  },
+  {
+    "revision": "3199cc31b3778ecf904c",
+    "url": "/robofriends-progressive-web-app/static/js/main.d09a4562.chunk.js"
+  },
+  {
+    "revision": "01f282f9406e637925e9",
+    "url": "/robofriends-progressive-web-app/static/js/runtime~main.f90088c6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-progressive-web-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
